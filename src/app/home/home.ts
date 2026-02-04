7464689b6347032013c43fc0ac21aa3d32c2d3f7
Fabricio Balani <fbalanibarros@hotmail.com>
2026-02-03 21:09:14 -0300

    Box Contacts

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -11,7 +11,12 @@ import { ProfileService } from '../services/profile-service';
 export class Home {
 
   constructor(private profileService: ProfileService){};
+
+  profile?: Profile;
   
+  ngOnInit() {
+    this.profile = this.profileService.getProfile();
+  }
   
 
 }
--- src/app/services/profile-service.ts
+++ src/app/services/profile-service.ts
@@ -11,7 +11,7 @@ export class ProfileService {
     const profile: Profile = {
       email: 'fbalanibarros@gmail.com',
       name: 'Fabricio Balani',
-      whatsApp: 'https://api.whatsapp.com/send?phone=5567992566216&text=Ol%C3%A1,%20estou%20no%20seu%20site%20e%20gostaria%20d%20emais%20informa%C3%A7%C3%B5es.',
+      whatsApp: 'https://api.whatsapp.com/send?phone=5567992566216&text=Ol%C3%A1,%20estou%20no%20seu%20site%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es.',
       linkedin:'https://www.linkedin.com/in/fabr%C3%ADcio-balani-b7a51b21a/'       
     }
     
